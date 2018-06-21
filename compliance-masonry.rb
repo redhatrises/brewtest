1839a722e3701b39f91f9340639e8a04889f6cd5
OpenControl <dev@open-control.org>
2018-06-21 15:17:38 -0600

    Brew formula update for testgorel version v1.8

--- compliance-masonry.rb
+++ compliance-masonry.rb
@@ -1,11 +1,11 @@
 class ComplianceMasonry < Formula
-  desc ""
+  desc "Compliance Masonry is a command-line interface (CLI) that allows users to construct certification documentation using the OpenControl Schema."
   homepage "https://github.com/opencontrol/compliance-masonry"
   url "https://github.com/redhatrises/testgorel/releases/download/v1.8/testgorel_1.8_darwin_amd64.tar.gz"
   version "1.8"
-  sha256 "1a39857d3895da21579ad89a2c03a05d94e29f5323c4a33c1b0214faabec6a90"
+  sha256 "4d89d4933ab0d64bf2391f1801e3c7f68b23e5bcd115d654d4769cfebf69e0a0"
 
   def install
-    compliance-masonry, masonry
+    bin.install "compliance-masonry", "masonry"
   end
 end
